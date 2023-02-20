2e77313c0e33376d89ac8fbdaa632fab2e789ea9
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2023-02-20 09:07:16 +0100

    latest build

---
+++ pages/api/generate.js
@@ -0,0 +1,30 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const basePromptPrefix =
+`
+Escribe unas palabras como el presidente de México, Andres Manuel Lopez Obrador, como si estuviera respondiendo desde su podium  en Palacio de Gobierno, en la Ciudad de México, tambien conocida como la mañanera.
+
+Titulo:
+`
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.90,
+    max_tokens: 500,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
